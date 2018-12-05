a2fd72f76deb552b708f6f28f2d39c011fd60957
Hidayat Ullah Khan <huk2209@gmail.com>
2018-12-06 05:13:00 +0530

    updated using jekyll freelancer theme

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,18 +1,19 @@
 <?php
+$email_address = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
    empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   !$email_address)
    {
 	echo "No arguments Provided!";
 	return false;
    }
 
 $name = $_POST['name'];
-$email_address = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-if ($email === FALSE) {
+if ($email_address === FALSE) {
     echo 'Invalid email';
     exit(1);
 }
@@ -21,7 +22,7 @@ $message = $_POST['message'];
 
 
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'huk2209@gmail.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
